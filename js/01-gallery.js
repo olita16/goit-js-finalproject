37fdd920c722c7354485de4cdd762440f4e1c1ef
olita16 <119816415+olita16@users.noreply.github.com>
2023-12-24 00:13:56 +0100

    1st and 2nd tasks, first commit

---
+++ js/01-gallery.js
@@ -0,0 +1,62 @@
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+const galleryImages = document.querySelectorAll(".gallery__image");
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+gallery.innerHTML = createMarkup(galleryItems);
+
+gallery.addEventListener("click", openOriginalIMG);
+let modalOpen = false;
+let instance;
+
+
+function openOriginalIMG(e) {
+  e.preventDefault();
+
+  const imgOriginal = e.target.closest(".gallery__link");
+  instance = basicLightbox.create(
+    `
+    <img src="${imgOriginal.getAttribute("href")}" width="800" height="600">
+  `,
+    {
+      onShow: () => {
+        modalOpen = true;
+        document.addEventListener("keydown", closeModal);
+      },
+      onClose: () => {
+        modalOpen = false;
+        document.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+
+function closeModal(event) {
+  if (event.key === "Escape" && modalOpen) {
+    instance.close();
+    modalOpen = false;
+    document.removeEventListener("keydown", closeModal);
+  }
+}
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,32 @@
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+const galleryImages = document.querySelectorAll(".gallery__image");
+
+function createMarkup(galleryItems) {
+  return (
+    galleryItems
+      .map(
+        ({ preview, original, description }) =>
+          `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+          src="${preview}" 
+          alt="${description}" />
+        </a>
+      </li>`
+      )
+      .join("")
+  );
+}
+
+gallery.innerHTML = createMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
